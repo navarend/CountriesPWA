92852c9c69415fb0518f26ff5234e97294bcfbe9
Roberto <navarend@gmail.com>
2019-05-08 16:36:05 -0700

    Change the app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,10 @@ import { Routes, RouterModule } from "@angular/router";
 import { CountryComponent } from "./pages/country/country.component";
 import { CountriesComponent } from "./pages/countries/countries.component";
 
-const routes: Routes =[
+const APP_ROUTES: Routes = [
   { path: '', component: CountriesComponent },
-  { path: 'country/:id', component: CountryComponent },
-  { path: '**', component: CountriesComponent }
+{ path: 'country/:id', component: CountryComponent },
+{ path: '**', component: CountriesComponent }
 ];
 
-@NgModule({
-  imports: [ RouterModule.forRoot ( routes ) ],
-  exports: [ RouterModule ]
-})
-export class AppRoutingModule { }
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, { useHash: true });
